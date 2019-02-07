18a9465b3705d1a5eec3baa0c9093ebdcc62f0f0
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-02-07 08:53:07 -0800

    finish

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <React.Fragment>
+        <header className="header">
+            <h1>(React) Google Books Search</h1>
+            <h2>Search for and Save Books of the Interest </h2>
+        </header>
+    </React.Fragment>
+  )
+}
+
+export default Header;
